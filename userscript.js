b256081fafd729f313949d86ed63c2a6f1e2bcc0
Aoshi Xu <196603834+Davidasx@users.noreply.github.com>
2025-05-07 14:37:32 +0800

    loadCaptcha: manually load captcha script

--- userscript.js
+++ userscript.js
@@ -16,6 +16,9 @@
     function ATKprint(num) {
         return Math.floor(num) + "." + (Math.floor((num - Math.floor(num)) * 10))
     }
+
+    function loadCaptcha() { var w = unsafeWindow, C = '___grecaptcha_cfg', cfg = w[C] = w[C] || {}, N = 'grecaptcha'; var gr = w[N] = w[N] || {}; gr.ready = gr.ready || function (f) { (cfg['fns'] = cfg['fns'] || []).push(f); }; w['__recaptcha_api'] = 'https://recaptcha.net/recaptcha/api2/'; (cfg['render'] = cfg['render'] || []).push('6LdeDqopAAAAAFhBk3q_TY7uB4QjU1QJ26viqZzm'); (cfg['clr'] = cfg['clr'] || []).push('true'); w['__google_recaptcha_client'] = true; var d = document, po = d.createElement('script'); po.type = 'text/javascript'; po.async = true; po.charset = 'utf-8'; var v = w.navigator, m = d.createElement('meta'); m.httpEquiv = 'origin-trial'; m.content = 'A6iYDRdcg1LVww9DNZEU+JUx2g1IJxSxk4P6F+LimR0ElFa38FydBqtz/AmsKdGr11ZooRgDPCInHJfGzwtR+A4AAACXeyJvcmlnaW4iOiJodHRwczovL3d3dy5yZWNhcHRjaGEubmV0OjQ0MyIsImZlYXR1cmUiOiJEaXNhYmxlVGhpcmRQYXJ0eVN0b3JhZ2VQYXJ0aXRpb25pbmczIiwiZXhwaXJ5IjoxNzU3OTgwODAwLCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ=='; if (v && v.cookieDeprecationLabel) { v.cookieDeprecationLabel.getValue().then(function (l) { if (l !== 'treatment_1.1' && l !== 'treatment_1.2' && l !== 'control_1.1') { d.head.prepend(m); } }); } else { d.head.prepend(m); } var m = d.createElement('meta'); m.httpEquiv = 'origin-trial'; m.content = '3NNj0GXVktLOmVKwWUDendk4Vq2qgMVDBDX+Sni48ATJl9JBj+zF+9W2HGB3pvt6qowOihTbQgTeBm9SKbdTwYAAABfeyJvcmlnaW4iOiJodHRwczovL3JlY2FwdGNoYS5uZXQ6NDQzIiwiZmVhdHVyZSI6IlRwY2QiLCJleHBpcnkiOjE3MzUzNDM5OTksImlzVGhpcmRQYXJ0eSI6dHJ1ZX0='; d.head.prepend(m); po.src = 'https://www.gstatic.com/recaptcha/releases/ItfkQiGBlJDHuTkOhlT3zHpB/recaptcha__zh_cn.js'; po.crossOrigin = 'anonymous'; po.integrity = 'sha384-UF8pAykZ+VBSDcjXDEt2VvikemnguufrcXs9KUn/cNlu5UOpsyb3P1RhkXBdRuLk'; var e = d.querySelector('script[nonce]'), n = e && (e['nonce'] || e.getAttribute('nonce')); if (n) { po.setAttribute('nonce', n); } var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); };
+    
     const auto_scroll = {
         _value: GM_getValue("auto_scroll", true),
         get value() {
@@ -411,6 +414,9 @@
         return 0
     }
     function DEFcalc(mob, rar) {
+        if(mob=="Angel Slime"){
+            return 0
+        }
         let returnValue = slotConvert(rar) * rarityATK(rar);
         if (mobConvert(mob) > 0) {
             returnValue = returnValue + mobConvert(mob) * rarityFactorMob(rar)
@@ -633,13 +639,7 @@
         if (testRarity > 4) {
             return false
         }
-        if (testRarity < 3) {
-            return false
-        }
-        if (testID === 33 && testRarity < 3) {
-            return false
-        }
-        if (testID === 34 && testRarity === 2) {
+        if (testID === 33) {
             return false
         }
         if (testID < 0 || testID > 100) {
@@ -1192,10 +1192,24 @@
             newMessageValue = ""
             document.getElementById("message").value = newMessageValue
         }
+        //使用方法：.loadcaptcha，加载reCaptcha
+        if (newMessageValue === ".loadcaptcha") {
+            loadCaptcha()
+            newMessageValue = ""
+            document.getElementById("message").value = newMessageValue
+        }
         //使用方法：.pos pos，如.pos 24可以打开City4
         if (newMessageValue.slice(0, 5) === ".pos ") {
             newMessageValue = newMessageValue.slice(6)
-            unsafeWindow.choosepos(parseInt(newMessageValue, 10))
+            function ourChoosePos(thepos){
+                grecaptcha.ready(function() {
+                    grecaptcha.execute("6LdeDqopAAAAAFhBk3q_TY7uB4QjU1QJ26viqZzm", {action: "submit"}).then(function(token) {
+                        var url =  "https://ruarua.ru/mob?pos=" + thepos + "&token=" + token;
+                        reloadPjax(url, 0, 0)
+                    });
+                });
+            }
+            ourChoosePos(parseInt(newMessageValue, 10))
             newMessageValue = ""
             document.getElementById("message").value = newMessageValue
         }
